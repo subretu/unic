b5c4cff8919f0530b1b83285f3f2bd87f8413c9b
miyahara <50367145+subretu@users.noreply.github.com>
2022-07-01 21:54:36 +0900

    case文に書き換え

--- test.py
+++
@@ -1,6 +0,0 @@
-import unit_converter
-
-
-aaa = unit_converter.ConvertTime()
-
-print(aaa.convert_millisecond(1905555, "s"))
--- unit_converter/time/convert_time.py
+++ unit_converter/time/convert_time.py
@@ -4,15 +4,16 @@ class ConvertTime:
 
     def convert_millisecond(self, data, time_unit):
         try:
-            if time_unit == "h":
-                return (data / 1000) / 3600
-            elif time_unit == "m":
-                return (data / 1000) / 60
-            elif time_unit == "s":
-                return data / 1000
-            elif time_unit == "ms":
-                return data
-            else:
-                raise Exception("Undefined unit time.")
+            match time_unit:
+                case "h":
+                    return (data / 1000) / 3600
+                case "m":
+                    return (data / 1000) / 60
+                case "s":
+                    return data / 1000
+                case "ms":
+                    return data
+                case _:
+                    raise Exception("Undefined unit time.")
         except Exception:
             raise
