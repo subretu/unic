af7cc531612990276540ddd77c3ce9bfcd9818e0
miyahara <50367145+subretu@users.noreply.github.com>
2022-07-03 22:44:08 +0900

    add convert function

--- src/unitconverter/time/convert_time.py
+++ src/unitconverter/time/convert_time.py
@@ -1,18 +1,63 @@
 class ConvertTime:
-    def __init__(self):
-        pass
-
-    def convert_millisecond(self, data, time_unit):
+    def convert_millisecond(self, msec, time_unit):
         try:
             match time_unit:
                 case "h":
-                    return (data / 1000) / 3600
+                    return (msec / 1000) / 3600
                 case "m":
-                    return (data / 1000) / 60
+                    return (msec / 1000) / 60
                 case "s":
-                    return data / 1000
+                    return msec / 1000
                 case "ms":
-                    return data
+                    return msec
+                case _:
+                    raise Exception("Undefined unit time.")
+        except Exception:
+            raise
+
+    def convert_second(self, sec, time_unit):
+        try:
+            match time_unit:
+                case "h":
+                    return sec / 3600
+                case "m":
+                    return sec / 60
+                case "s":
+                    return sec
+                case "ms":
+                    return sec * 1000
+                case _:
+                    raise Exception("Undefined unit time.")
+        except Exception:
+            raise
+
+    def convert_minutes(self, min, time_unit):
+        try:
+            match time_unit:
+                case "h":
+                    return min / 60
+                case "m":
+                    return min
+                case "s":
+                    return min * 60
+                case "ms":
+                    return min * 60 * 1000
+                case _:
+                    raise Exception("Undefined unit time.")
+        except Exception:
+            raise
+
+    def convert_hour(self, hr, time_unit):
+        try:
+            match time_unit:
+                case "h":
+                    return hr
+                case "m":
+                    return hr * 60
+                case "s":
+                    return hr * 3600
+                case "ms":
+                    return hr * 3600 * 1000
                 case _:
                     raise Exception("Undefined unit time.")
         except Exception:
