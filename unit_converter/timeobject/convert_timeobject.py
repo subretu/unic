4859b0c864d59242949d5f4a6edd06a54edfb819
miyahara <50367145+subretu@users.noreply.github.com>
2022-07-09 22:24:54 +0900

    add timezone process

--- unit_converter/timeobject/convert_timeobject.py
+++ unit_converter/timeobject/convert_timeobject.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone, timedelta
 import time
 from .config import settings
 
@@ -8,35 +8,25 @@ class ConvertTimeObject:
         try:
             if len(kwargs) == 0:
                 if len(str(abs(data))) == 10:
-                    dt_timestamp = datetime.datetime.fromtimestamp(data, datetime.timezone.utc)
+                    dt_timestamp = datetime.fromtimestamp(data, timezone.utc)
                     return dt_timestamp
                 elif len(str(abs(data))) == 13:
-                    dt_timestamp = datetime.datetime.fromtimestamp(
-                        data / 1000, datetime.timezone.utc
-                    )
+                    dt_timestamp = datetime.fromtimestamp(data / 1000, timezone.utc)
                     return dt_timestamp
                 else:
                     raise Exception("Unixtime digits error.")
             elif len(kwargs) == 1:
                 for key in kwargs.keys():
                     if key == "tz" and len(str(abs(data))) == 10:
-                        dt_timestamp = datetime.datetime.fromtimestamp(
+                        dt_timestamp = datetime.fromtimestamp(
                             data,
-                            datetime.timezone(
-                                datetime.timedelta(
-                                    hours=settings.TIMEZONE[kwargs["tz"]]
-                                )
-                            ),
+                            timezone(timedelta(hours=settings.TIMEZONE[kwargs["tz"]])),
                         )
                         return dt_timestamp
                     elif key == "tz" and len(str(abs(data))) == 13:
-                        dt_timestamp = datetime.datetime.fromtimestamp(
+                        dt_timestamp = datetime.fromtimestamp(
                             data / 1000,
-                            datetime.timezone(
-                                datetime.timedelta(
-                                    hours=settings.TIMEZONE[kwargs["tz"]]
-                                )
-                            ),
+                            timezone(timedelta(hours=settings.TIMEZONE[kwargs["tz"]])),
                         )
                         return dt_timestamp
                     else:
@@ -49,16 +39,38 @@ class ConvertTimeObject:
             raise
 
     def convert_unixtime(self, data, **kwargs):
-        except_milisecond = data[0:19]
-        milisecond = data[20:23]
-        dt = datetime.datetime.strptime(except_milisecond, "%Y-%m-%d %H:%M:%S")
-        dt_timestamp = datetime.datetime(
-            dt.year,
-            dt.month,
-            dt.day,
-            dt.hour,
-            dt.minute,
-            dt.second,
-        )
-        dt_unixtime = str(int(time.mktime(dt_timestamp.timetuple()))) + milisecond
-        return dt_unixtime
+        try:
+            except_milisecond = data[0:19]
+            milisecond = data[20:23]
+            if len(kwargs) == 0:
+                dt_timestamp = datetime.strptime(except_milisecond, "%Y-%m-%d %H:%M:%S")
+                dt_unixtime = (
+                    str(int(dt_timestamp.replace(tzinfo=timezone.utc).timestamp()))
+                    + milisecond
+                )
+                return dt_unixtime
+            elif len(kwargs) == 1:
+                for key in kwargs.keys():
+                    if key == "tz":
+                        dt_timestamp = datetime.strptime(
+                            except_milisecond, "%Y-%m-%d %H:%M:%S"
+                        ) + timedelta(hours=settings.TIMEZONE[kwargs["tz"]])
+                        dt_unixtime = (
+                            str(
+                                int(
+                                    dt_timestamp.replace(
+                                        tzinfo=timezone.utc
+                                    ).timestamp()
+                                )
+                            )
+                            + milisecond
+                        )
+                        return dt_unixtime
+                    else:
+                        raise Exception(
+                            "Parameter name error or Unixtime digits error."
+                        )
+            else:
+                raise Exception("Too many parameter.")
+        except Exception:
+            raise
