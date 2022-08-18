92afb937df09dc17f5505107472fe61dad9dbcd5
miyahara <50367145+subretu@users.noreply.github.com>
2022-08-18 23:36:32 +0900

    add testcase

--- tests/test_convert_datetime.py
+++ tests/test_convert_datetime.py
@@ -24,12 +24,19 @@ class TestConvertDatetime:
             2022, 7, 17, 00, 31, 34, tzinfo=timezone(timedelta(seconds=32400))
         )
 
-    def test_convert_datetime_parameter_error(self):
+    def test_convert_datetime_parameter_name_error(self):
         with pytest.raises(Exception) as e:
             test_timeobject = timeobject.TimeObject()
             _ = test_timeobject.convert_datetime(1657985494, timezone="Asia/Tokyo")
 
         assert str(e.value) == "Parameter name not defined."
+
+    def test_convert_datetime_parameter_value_error(self):
+        with pytest.raises(Exception) as e:
+            test_timeobject = timeobject.TimeObject()
+            _ = test_timeobject.convert_datetime(1657985494, tz="Asia/Osaka")
+
+        assert str(e.value) == "Invalid parameter value."
 
     def test_convert_datetime_parameter_number_error(self):
         with pytest.raises(Exception) as e:
