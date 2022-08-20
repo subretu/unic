373b8db3d75e6943273ac1d6bce5ee96cc4c5c53
miyahara <50367145+subretu@users.noreply.github.com>
2022-08-20 15:33:25 +0900

    refactor

--- unicorn/timeobject/timeobject.py
+++ unicorn/timeobject/timeobject.py
@@ -6,63 +6,57 @@ from ..utils import check_parameter
 class TimeObject:
     def convert_datetime(self, data, **kwargs):
         try:
-            if len(kwargs) <= 1:
-                check_parameter.check_parameter_name(kwargs)
+            check_parameter.check_number(kwargs)
+            check_parameter.check_name(kwargs)
+            if len(kwargs) == 1:
+                check_parameter.check_value(kwargs["tz"])
 
-                if len(kwargs) == 1:
-                    check_parameter.check_parameter_value(kwargs["tz"])
+            timezone_hour = (
+                lambda: settings.TIMEZONE[kwargs["tz"]] if len(kwargs) == 1 else 0
+            )
 
-                timezone_hour = (
-                    lambda: settings.TIMEZONE[kwargs["tz"]] if len(kwargs) == 1 else 0
+            digits = self._count_digits(data)
+
+            if digits == 10:
+                dt_timestamp = datetime.fromtimestamp(
+                    data,
+                    timezone(timedelta(hours=timezone_hour())),
                 )
-
-                digits = self._count_digits(data)
-
-                if digits == 10:
-                    dt_timestamp = datetime.fromtimestamp(
-                        data,
-                        timezone(timedelta(hours=timezone_hour())),
-                    )
-                    return dt_timestamp
-                elif digits == 13:
-                    dt_timestamp = datetime.fromtimestamp(
-                        data / 1000,
-                        timezone(timedelta(hours=timezone_hour())),
-                    )
-                    return dt_timestamp
-            else:
-                raise Exception("Too many parameter.")
+                return dt_timestamp
+            elif digits == 13:
+                dt_timestamp = datetime.fromtimestamp(
+                    data / 1000,
+                    timezone(timedelta(hours=timezone_hour())),
+                )
+                return dt_timestamp
         except Exception:
             raise
 
     def convert_date(self, data, **kwargs):
         try:
-            if len(kwargs) <= 1:
-                check_parameter.check_parameter_name(kwargs)
+            check_parameter.check_number(kwargs)
+            check_parameter.check_name(kwargs)
+            if len(kwargs) == 1:
+                check_parameter.check_value(kwargs["tz"])
 
-                if len(kwargs) == 1:
-                    check_parameter.check_parameter_value(kwargs["tz"])
+            timezone_hour = (
+                lambda: settings.TIMEZONE[kwargs["tz"]] if len(kwargs) == 1 else 0
+            )
 
-                timezone_hour = (
-                    lambda: settings.TIMEZONE[kwargs["tz"]] if len(kwargs) == 1 else 0
+            digits = self._count_digits(data)
+
+            if digits == 10:
+                dt_timestamp = datetime.fromtimestamp(
+                    data,
+                    timezone(timedelta(hours=timezone_hour())),
                 )
-
-                digits = self._count_digits(data)
-
-                if digits == 10:
-                    dt_timestamp = datetime.fromtimestamp(
-                        data,
-                        timezone(timedelta(hours=timezone_hour())),
-                    )
-                    return dt_timestamp.date()
-                elif digits == 13:
-                    dt_timestamp = datetime.fromtimestamp(
-                        data / 1000,
-                        timezone(timedelta(hours=timezone_hour())),
-                    )
-                    return dt_timestamp.date()
-            else:
-                raise Exception("Too many parameter.")
+                return dt_timestamp.date()
+            elif digits == 13:
+                dt_timestamp = datetime.fromtimestamp(
+                    data / 1000,
+                    timezone(timedelta(hours=timezone_hour())),
+                )
+                return dt_timestamp.date()
         except Exception:
             raise
 
--- unicorn/utils/check_parameter.py
+++ unicorn/utils/check_parameter.py
@@ -1,14 +1,21 @@
 from ..config import settings
 
 
-def check_parameter_name(parameter_name):
+def check_number(paramete):
+    if len(paramete) <= 1:
+        pass
+    else:
+        raise Exception("Too many parameter.")
+
+
+def check_name(parameter_name):
     if (len(parameter_name) == 0) or ("tz" in parameter_name):
         pass
     else:
         raise Exception("Invalid parameter name.")
 
 
-def check_parameter_value(parameter_value):
+def check_value(parameter_value):
     if parameter_value in settings.TIMEZONE:
         pass
     else:
