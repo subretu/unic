dd2e5a08fa909c316852f3c08cdc28b20bf17e2f
miyahara <50367145+subretu@users.noreply.github.com>
2022-09-13 22:16:15 +0900

    add type hint

--- unicorn/utils/check_parameter.py
+++ unicorn/utils/check_parameter.py
@@ -1,21 +1,21 @@
 from ..config import settings
 
 
-def check_number(paramete):
-    if len(paramete) <= 1:
+def check_number(parameter: dict) -> None:
+    if len(parameter) <= 1:
         pass
     else:
         raise Exception("Too many parameter.")
 
 
-def check_name(parameter_name):
+def check_name(parameter_name: dict) -> None:
     if (len(parameter_name) == 0) or ("tz" in parameter_name):
         pass
     else:
         raise Exception("Invalid parameter name.")
 
 
-def check_value(parameter_value):
+def check_value(parameter_value: str) -> None:
     if parameter_value in settings.TIMEZONE:
         pass
     else:
