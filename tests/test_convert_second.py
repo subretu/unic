a8e9d7cfaaff4d2e47243e038e5d4c24877df8d6
miyahara <50367145+subretu@users.noreply.github.com>
2022-12-01 21:51:18 +0900

    refactor

--- tests/test_convert_second.py
+++ tests/test_convert_second.py
@@ -1,12 +1,6 @@
 import pytest
 from unicorn.unit import timeunit
 
-convert_second_msec_case = [
-    (8, "msec", 0.008),
-    (9, "msec", 0.009),
-    (10, "msec", 0.01)
-]
-
 
 class TestConverterSecond:
     def test_convert_second_msec(self):
@@ -15,7 +9,10 @@ class TestConverterSecond:
 
         assert result == 0.008
 
-    @pytest.mark.parametrize("msec, unit, expect", convert_second_msec_case)
+    @pytest.mark.parametrize(
+        "msec, unit, expect",
+        [(8, "msec", 0.008), (9, "msec", 0.009), (10, "msec", 0.01)],
+    )
     def test_convert_second_msec2(self, msec, unit, expect):
         test_timeunit = timeunit.TimeUnit()
         result = test_timeunit.convert_second(msec, unit)
