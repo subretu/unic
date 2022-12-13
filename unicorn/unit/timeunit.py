edea3ee23fd92e9bb69c54964615bc901878fb8c
miyahara <50367145+subretu@users.noreply.github.com>
2022-12-14 00:38:37 +0900

    fix

--- unicorn/unit/timeunit.py
+++ unicorn/unit/timeunit.py
@@ -9,7 +9,7 @@ class TimeUnitrModel(BaseModel):
     def unit_check(cls, v):
         if v not in ["msec", "sec", "min", "hour"]:
             raise ValueError("Undefined unit")
-        return v.title()
+        return v
 
 
 def _init(data: int, unit: str):
@@ -24,14 +24,14 @@ class TimeUnit:
     def convert_millisecond(self, data: int, unit: str) -> int:
         try:
             input_data = _init(data, unit)
-            if unit == "msec":
-                return data
-            elif unit == "sec":
-                return data * 1000
-            elif unit == "min":
-                return data * 60 * 1000
-            elif unit == "hour":
-                return data * 3600 * 1000
+            if input_data.unit == "msec":
+                return input_data.data
+            elif input_data.unit == "sec":
+                return input_data.data * 1000
+            elif input_data.unit == "min":
+                return input_data.data * 60 * 1000
+            elif input_data.unit == "hour":
+                return input_data.data * 3600 * 1000
             else:
                 raise Exception("Undefined unit time.")
         except Exception:
