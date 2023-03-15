43d8fd2693510dd088c8da1edc5df8672be1c400
miyahara <50367145+subretu@users.noreply.github.com>
2023-03-15 13:36:17 +0900

    refactor

--- unicorn/timeobject/timeobject.py
+++ unicorn/timeobject/timeobject.py
@@ -4,40 +4,37 @@ from ..utils import check_parameter, config_parser
 
 class TimeObject:
     def convert(self, data: int, **kwargs: any) -> date:
-        try:
-            self._check_parameter_name(kwargs)
+        self.check_parameter_name(kwargs)
+        tz = kwargs.get("tz", None)
+        target = kwargs["target"]
 
-            if len(kwargs) == 2:
-                check_parameter.check_value(kwargs["tz"])
-                parameter = config_parser.parse_toml("./unicorn/configs/timezone.toml")
-                timezone_hour = parameter[kwargs["tz"]]["value"]
-            else:
-                timezone_hour = 0
+        if tz:
+            check_parameter.check_value(tz)
+            parameter = config_parser.parse_toml("./unicorn/configs/timezone.toml")
+            timezone_hour = parameter[tz]["value"]
+        else:
+            timezone_hour = 0
 
-            digits = self._count_digits(data)
+        digits = self.count_digits(data)
 
-            dt_timestamp = self._convert_timestamp_by_digits(
-                data, digits, timezone_hour
-            )
+        dt_timestamp = self.convert_timestamp_by_digits(data, digits, timezone_hour)
 
-            if kwargs["target"] == "datetime":
-                return dt_timestamp
-            elif kwargs["target"] == "date":
-                return dt_timestamp.date()
-            else:
-                raise
-        except Exception:
-            raise
+        if target == "datetime":
+            return dt_timestamp
+        elif target == "date":
+            return dt_timestamp.date()
+        else:
+            raise ValueError("Invalid target")
 
-    def _count_digits(self, data: int) -> int:
+    def count_digits(self, data: int) -> int:
         digits = len(str(abs(data)))
 
         if digits == 10 or digits == 13:
             return digits
         else:
-            raise Exception("Unixtime digits is 10 or 13.")
+            raise ValueError("Unixtime digits is 10 or 13.")
 
-    def _convert_timestamp_by_digits(
+    def convert_timestamp_by_digits(
         self, data: int, digits: int, timezone_hour: int
     ) -> datetime:
         if digits == 10:
@@ -53,14 +50,14 @@ class TimeObject:
             )
             return dt_timestamp
 
-    def _check_parameter_name(self, parameter_name: dict) -> None:
+    def check_parameter_name(self, parameter_name: dict) -> None:
         if (len(parameter_name) == 1) and ("target" in parameter_name):
-            pass
+            return
         elif (
             (len(parameter_name) == 2)
             and ("target" in parameter_name)
             and ("tz" in parameter_name)
         ):
-            pass
+            return
         else:
-            raise Exception("Invalid parameter name.")
+            raise ValueError("Invalid parameter name.")
