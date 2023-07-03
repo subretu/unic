8cecd59774925f33412750c39198b4c3c1ad7574
miyahara <50367145+subretu@users.noreply.github.com>
2023-07-04 00:25:28 +0900

    fix test

--- tests/test_convert_hour.py
+++ tests/test_convert_hour.py
@@ -40,7 +40,8 @@ class TestConverterHour:
 
         error_msg = """1 validation error for TimeUnitModel
 to_unit
-  Invalid to_unit name: hr. Allowed values are ['msec', 'sec', 'min', 'hour']. (type=value_error)"""
+  Value error, Invalid to_unit name: hr. Allowed values are ['msec', 'sec', 'min', 'hour']. [type=value_error, input_value='hr', input_type=str]
+    For further information visit https://errors.pydantic.dev/2.0.1/v/value_error"""
         assert str(e.value) == error_msg
 
     def test_convert_hour_fail_parameter_count_shortage_from_unit(self):
--- tests/test_convert_minute.py
+++ tests/test_convert_minute.py
@@ -40,7 +40,8 @@ class TestConverterMinute:
 
         error_msg = """1 validation error for TimeUnitModel
 to_unit
-  Invalid to_unit name: minute. Allowed values are ['msec', 'sec', 'min', 'hour']. (type=value_error)"""
+  Value error, Invalid to_unit name: minute. Allowed values are ['msec', 'sec', 'min', 'hour']. [type=value_error, input_value='minute', input_type=str]
+    For further information visit https://errors.pydantic.dev/2.0.1/v/value_error"""
         assert str(e.value) == error_msg
 
     def test_convert_minute_fail_parameter_count_shortage(self):
