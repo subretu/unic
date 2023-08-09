197e33d8d6f0c7ba8a093a560bfde3929c273918
miyahara <50367145+subretu@users.noreply.github.com>
2023-08-09 12:48:04 +0900

    rename classname and filename

--- tests/test_convert_datetime.py
+++ tests/test_convert_datetime.py
@@ -1,23 +1,23 @@
 import pytest
 from datetime import datetime, timezone, timedelta
-from unic.time_unit.datetime import _datetime
+from unic.time_unit.datetime import datetime_model
 
 
 class TestConvertDatetime:
     def test_convert_datetime_utc_10digits(self):
-        test_timeobject = _datetime.Datetime()
+        test_timeobject = datetime_model.DatetimeModel()
         result = test_timeobject.convert(1657985494, target="datetime")
 
         assert result == datetime(2022, 7, 16, 15, 31, 34, tzinfo=timezone.utc)
 
     def test_convert_datetime_utc_13digits(self):
-        test_timeobject = _datetime.Datetime()
+        test_timeobject = datetime_model.DatetimeModel()
         result = test_timeobject.convert(1657985494123, target="datetime")
 
         assert result == datetime(2022, 7, 16, 15, 31, 34, 123000, tzinfo=timezone.utc)
 
     def test_convert_datetime_jst(self):
-        test_timeobject = _datetime.Datetime()
+        test_timeobject = datetime_model.DatetimeModel()
         result = test_timeobject.convert(1657985494, target="datetime", tz="Asia/Tokyo")
 
         assert result == datetime(
@@ -26,7 +26,7 @@ class TestConvertDatetime:
 
     def test_convert_datetime_parameter_name_error(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(
                 1657985494, target="datetime", timezone="Asia/Tokyo"
             )
@@ -35,14 +35,14 @@ class TestConvertDatetime:
 
     def test_convert_datetime_parameter_value_error(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(1657985494, target="datetime", tz="Asia/Osaka")
 
         assert str(e.value) == "Invalid parameter value."
 
     def test_convert_datetime_parameter_number_error(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(
                 1657985494, target="datetime", tz="Asia/Tokyo", hoge=123456
             )
@@ -51,21 +51,21 @@ class TestConvertDatetime:
 
     def test_convert_datetime_digits_error_patarn1(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(16579854949, target="datetime")
 
         assert str(e.value) == "Unixtime digits is 10 or 13."
 
     def test_convert_datetime_digits_error_patarn2(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(165798549412, target="datetime")
 
         assert str(e.value) == "Unixtime digits is 10 or 13."
 
     def test_convert_datetime_digits_error_patarn3(self):
         with pytest.raises(Exception) as e:
-            test_timeobject = _datetime.Datetime()
+            test_timeobject = datetime_model.DatetimeModel()
             _ = test_timeobject.convert(165798549, target="datetime", tz="Asia/Tokyo")
 
         assert str(e.value) == "Unixtime digits is 10 or 13."
--- unic/__init__.py
+++ unic/__init__.py
@@ -1,5 +1,5 @@
 from .unit.timeunit import *
-from .time_unit.datetime._datetime import *
+from .time_unit.datetime.datetime_model import *
 from .unixtime.unixtime import *
 
 __version__ = "0.1.1"
