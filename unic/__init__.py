db7b7052280dfaf2cd81ba5bf6add92b8f96c7cf
miyahara <50367145+subretu@users.noreply.github.com>
2023-09-29 23:00:12 +0900

    version up

--- unic/__init__.py
+++ unic/__init__.py
@@ -2,4 +2,4 @@ from .time_unit.time.time_model import *
 from .time_unit.datetime.datetime_model import *
 from .time_unit.unixtime.unixtime_model import *
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
