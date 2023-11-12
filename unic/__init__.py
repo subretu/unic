d453de61d7c0c459078f697dfc78c26917d04a0e
miyahara <50367145+subretu@users.noreply.github.com>
2023-11-12 21:46:06 +0900

    Changed handling for undefined model inputs

--- unic/__init__.py
+++ unic/__init__.py
@@ -1,7 +1,7 @@
 from .time_unit.time.time_model import TimeModel
 from .time_unit.datetime.datetime_model import DatetimeModel
 from .time_unit.unixtime.unixtime_model import UnixtimeModel
-from typing import Union
+from typing import List, Union
 
 _convert_model_list = {
     "time": TimeModel(),
@@ -13,8 +13,14 @@ _convert_model_list = {
 def load_model(
     name: str,
 ) -> Union[TimeModel, DatetimeModel, UnixtimeModel]:
-    try:
+    if name in _convert_model_list:
         convert_model = _convert_model_list[name]
         return convert_model
-    except KeyError:
-        raise ValueError("Invalid model name.")
+    else:
+        raise RuntimeError(
+            f"Model {name} not found, available models = {available_models()}"
+        )
+
+
+def available_models() -> List[str]:
+    return list(_convert_model_list.keys())
