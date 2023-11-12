59be9798e6e6b18d6d6bbb7b51e388ea32d625e5
miyahara <50367145+subretu@users.noreply.github.com>
2023-11-12 14:41:48 +0900

    Model instantiation changed from load_model

--- unic/__init__.py
+++ unic/__init__.py
@@ -1,5 +1,20 @@
-from .time_unit.time.time_model import *
-from .time_unit.datetime.datetime_model import *
-from .time_unit.unixtime.unixtime_model import *
+from .time_unit.time.time_model import TimeModel
+from .time_unit.datetime.datetime_model import DatetimeModel
+from .time_unit.unixtime.unixtime_model import UnixtimeModel
+from typing import Union
 
-__version__ = "0.2.4"
+_convert_model_list = {
+    "time": TimeModel(),
+    "datetime": DatetimeModel(),
+    "unixtime": UnixtimeModel(),
+}
+
+
+def load_model(
+    name: str,
+) -> Union[TimeModel, DatetimeModel, UnixtimeModel]:
+    try:
+        convert_model = _convert_model_list[name]
+        return convert_model
+    except:
+        raise ValueError("Invalid model name.")
