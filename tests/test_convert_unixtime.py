b8eed8c100df5826cddfad1e7efc9788c4e6851b
miyahara <50367145+subretu@users.noreply.github.com>
2023-11-13 00:15:13 +0900

    format

--- tests/test_convert_unixtime.py
+++ tests/test_convert_unixtime.py
@@ -4,26 +4,26 @@ import unic
 
 class TestConvertUnixtime:
     def test_convert_unixtime_utc(self):
-        test_unixtime= unic.load_model("unixtime")
+        test_unixtime = unic.load_model("unixtime")
         result = test_unixtime.convert("2022-07-18 13:49:00")
 
         assert result == 1658152140
 
     def test_convert_unixtime_except_milisecond_utc(self):
-        test_unixtime= unic.load_model("unixtime")
+        test_unixtime = unic.load_model("unixtime")
         result = test_unixtime.convert("2022-07-18 13:49:00.123")
 
         assert result == 1658152140123
 
     def test_convert_unixtime_jst(self):
-        test_unixtime= unic.load_model("unixtime")
+        test_unixtime = unic.load_model("unixtime")
         result = test_unixtime.convert("2022-07-18 13:49:00", tz="Asia/Tokyo")
 
         assert result == 1658184540
 
     def test_convert_unixtime_parameter_number_error(self):
         with pytest.raises(Exception) as e:
-            test_unixtime= unic.load_model("unixtime")
+            test_unixtime = unic.load_model("unixtime")
             _ = test_unixtime.convert(
                 "2022-07-18 13:49:00", tz="Asia/Tokyo", hoge=123456
             )
@@ -35,7 +35,7 @@ class TestConvertUnixtime:
 
     def test_convert_unixtime_parameter_error(self):
         with pytest.raises(Exception) as e:
-            test_unixtime= unic.load_model("unixtime")
+            test_unixtime = unic.load_model("unixtime")
             _ = test_unixtime.convert("2022/07/18 13:49:00", tz="Asia/Tokyo")
 
         error_msg = "Value error, '2022/07/18 13:49:00' is invalid date format. Allowed formats are ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M:%S.%f']."
