e8119737104bc7912df304beee27a3e348ba6821
miyahara <50367145+subretu@users.noreply.github.com>
2023-12-12 23:54:24 +0900

    Add test

--- tests/test_convert_metric_system.py
+++ tests/test_convert_metric_system.py
@@ -6,6 +6,8 @@ class TestConverterMetricSystem:
     @pytest.mark.parametrize(
         "data, from_unit, to_unit,  expect",
         [
+            (1, "nm", "mm", 0.000001),
+            (3, "um", "cm", 0.00030000000000000003),
             (5, "mm", "cm", 0.5),
             (15, "mm", "m", 0.015),
             (10, "cm", "mm", 100),
@@ -21,3 +23,30 @@ class TestConverterMetricSystem:
         )
 
         assert result == expect
+
+    def test_convert_metric_system_fail(self):
+        with pytest.raises(Exception) as e:
+            test_metric_system_unit = unic.load_model("metric_system")
+            _ = test_metric_system_unit.convert(35, from_unit="mm", to_unit="MM")
+
+        error_msg = "Value error, MM is invalid value for parameter: to_unit. Allowed values are ['nm', 'um', 'mm', 'cm', 'm', 'km', 'Mm', 'Gm', 'Tm']."
+
+        assert str(e.value) == error_msg
+
+    def test_convert_metric_system_faill_parameter_count_shortage_from_unit(self):
+        with pytest.raises(Exception) as e:
+            test_metric_system_unit = unic.load_model("metric_system")
+            _ = test_metric_system_unit.convert(10, to_unit="cm")
+
+        error_msg = "The 'from_unit' argument is required."
+
+        assert str(e.value) == error_msg
+
+    def test_convert_metric_system_fail_parameter_count_shortage_to_unit(self):
+        with pytest.raises(Exception) as e:
+            test_metric_system_unit = unic.load_model("metric_system")
+            _ = test_metric_system_unit.convert(100, from_unit="m")
+
+        error_msg = "The 'to_unit' argument is required."
+
+        assert str(e.value) == error_msg
