be5be65118084a6e383af92e0e94dad1ccf38735
miyahara <50367145+subretu@users.noreply.github.com>
2024-05-16 23:56:19 +0900

    Cache time zone settings

--- unic/time_unit/unixtime/unixtime_model.py
+++ unic/time_unit/unixtime/unixtime_model.py
@@ -5,16 +5,16 @@ from unic.time_unit.validators import validators
 
 
 class UnixtimeModel:
+    def __init__(self):
+        self.timezone_parameters = config_parser.parse_toml("timezone")
+
     def convert(self, data: str, tz: str = None) -> int:
         try:
             input_data = validators.UnixtimeModelValidator(data=data, tz=tz)
         except ValidationError as e:
             raise ValueError(e.errors()[0]["msg"])
 
-        timezone_hour = 0
-        if tz:
-            parameter = config_parser.parse_toml("timezone")
-            timezone_hour = parameter[tz]["value"]
+        timezone_hour = self.timezone_parameters.get(tz, {}).get("value", 0)
 
         except_milisecond = input_data.data[0:19]
         milisecond = input_data.data[20:23]
